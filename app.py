ea2cc58b94b94f3df1ba931b12b377c6dba09445
Amy George <ageorge@alumni.ubc.ca>
2021-04-10 19:03:16 -0700

    run the app.py file to collect data from the scraper.py file

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask_cors import CORS
+import pandas as pd
+import json
+
+from search import return_relevant_recipes
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+CORS(app)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    return (
+        "Welcome to the RECIPERFECT search API!<br/>")
+
+@app.route("/api/search/<query>")
+def search_query(query=None):
+
+    try:
+        results = return_relevant_recipes(query)
+        df_to_dict = results.to_dict('r')
+        data = json.dumps(df_to_dict, ensure_ascii=False, indent=4)
+
+        return (
+            data
+        )
+
+    except Exception as e:
+        return (
+            f"{e}"
+    )
+
+if __name__ == '__main__':
+    app.run()
